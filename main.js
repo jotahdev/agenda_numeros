475087ee6771a82ab17490e72a7ca6daadc8f509
joao <joaomarcos56262@gmailcom>
2024-01-09 09:20:36 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+    function adicionarContato() {
+        var nome = document.getElementById('nome').value;
+        var telefone = document.getElementById('telefone').value;
+
+        if (nome && telefone) {
+            var tabela = document.getElementById('tabelaContatos').getElementsByTagName('tbody')[0];
+            var novaLinha = tabela.insertRow(tabela.rows.length);
+            var celulaNome = novaLinha.insertCell(0);
+            var celulaTelefone = novaLinha.insertCell(1);
+
+            celulaNome.innerHTML = nome;
+            celulaTelefone.innerHTML = telefone;
+
+            // Limpar os campos do formulário após adicionar o contato
+            document.getElementById('nome').value = '';
+            document.getElementById('telefone').value = '';
+        } else {
+            alert('Por favor, preencha todos os campos.');
+        }
+    }
